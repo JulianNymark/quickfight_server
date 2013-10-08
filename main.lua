881c963432b43055df311d674e32b202e45382a9
Julian Hisdal Nymark <julianymark@gmail.com>
2013-10-08 22:38:39 +0200

    tab indent space lol

--- main.lua
+++ main.lua
@@ -1,17 +1,17 @@
 server = {}
 
 function server.init( arg )
-	state = "running"
-	-- require "common"
-	require "socket"
+    state = "running"
+    -- require "common"
+    require "socket"
 end
 
 function server.update( dt )
-	print("server running!")
+    print("server running!")
 end
 
 function server.quit()
-	print("server exit")
+    print("server exit")
 end
 
 -- init, start thread for prompt & server update()
@@ -19,12 +19,12 @@ server.init( arg )
 
 -- run prompt
 while state == "running" do
-	io.write(string.format("> "))
-	input = io.read()
-	print(input)
-	
-	-- exit
-	if input == "exit" or input == "quit" then
-		state = "stopping"
-	end
+    io.write(string.format("> "))
+    input = io.read()
+    print(input)
+    
+    -- exit
+    if input == "exit" or input == "quit" then
+	state = "stopping"
+    end
 end
