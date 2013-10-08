6bdb68f41824c9bd7aae5da827365a27c8df81d8
Julian Hisdal Nymark <julianymark@gmail.com>
2013-10-08 21:07:18 +0200

    initial commit

---
+++ main.lua
@@ -0,0 +1,30 @@
+server = {}
+
+function server.init( arg )
+	state = "running"
+	-- require "common"
+	require "socket"
+end
+
+function server.update( dt )
+	print("server running!")
+end
+
+function server.quit()
+	print("server exit")
+end
+
+-- init, start thread for prompt & server update()
+server.init( arg )
+
+-- run prompt
+while state == "running" do
+	io.write(string.format("> "))
+	input = io.read()
+	print(input)
+	
+	-- exit
+	if input == "exit" or input == "quit" then
+		state = "stopping"
+	end
+end
